581574c129fd43084d540e5b93aed6eb532d3c90
Matthias Schartner <mschartn@vievs.geo.tuwien.ac.at>
2018-11-22 15:58:05 +0100

    symlink for VieVS Server

---
+++ CODE/VLBI/MISC/symlinkVievsServer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+declare -a folders=("DATA/NGS" "DATA/vgosDB" "TRP/VMF1" "TRP/VMF3" "TRP/GRAD" "TRP/LHG" "TRP/RAYTRACING_DATA" "ATM/APL_VIENNA" "ATM/GSFC")
+
+for i in "${folders[@]}" 
+do
+	echo "link $i"
+	rm -r ../../../VLBI/$i/*  > /dev/null 2>&1
+	ln -sfn /home/members/vievs/VLBI/$i/* ../../../VLBI/$i
+done
+
+
+declare -a files=("EOP")
+for i in "${files[@]}" 
+do
+	echo "link $i"
+	rm -r ../../../VLBI/$i/*.txt  > /dev/null 2>&1
+	ln -sfn /home/members/vievs/VLBI/$i/*.txt ../../../VLBI/$i
+done
+
+
+
+
+
+
