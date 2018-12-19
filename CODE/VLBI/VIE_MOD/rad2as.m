f8cdb87dc2571397d760dae13f4ca5986ba37b41
Sigrid Böhm <sigrid.boehm@tuwien.ac.at>
2018-12-19 15:11:38 +0100

    transition to COMMON repo and a bit of decluttering

--- CODE/VLBI/VIE_MOD/rad2as.m
+++
@@ -1,7 +0,0 @@
-function as=rad2as(rad)
-% Converts radians to seconds of arc.
-% Useage:  as=rad2as(rad)
-% Input:   rad - angle in radians
-% Output:  as  - angle in seconds of arc
-
-as=rad.*180*3600/pi;
--- CODE/VLBI/VIE_MOD/rad2mas.m
+++
@@ -1,7 +0,0 @@
-function mas=rad2mas(rad)
-% Converts radians to milliseconds of arc.
-% Useage:  mas=rad2mas(rad)
-% Input:   rad - angle in radians
-% Output:  mas  - angle in milliseconds of arc
-
-mas=rad.*180*3600/pi*1000;
