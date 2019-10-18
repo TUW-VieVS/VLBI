9dec45c9b4d7d00fe60d3382f1724993be87ed25
Markus Mikschi <mmikschi@vievs.geo.tuwien.ac.at>
2019-10-18 15:38:21 +0200

    Updated mk_list to work with VGOS and added REQOPT option. Updated documentation as well.

--- CODE/MISC/mk_list.m
+++ CODE/MISC/mk_list.m
@@ -1,11 +1,12 @@
 % ************************************************************************
+% Attention: When saving the list of the sessions in a .mat file to be used
+%            in vievs the name of the list has to be "process_list" as it
+%            is hard-coded in vievs. 
 % Description:
 %    Tool to make process-lists for VieVS. Reads information about the
 %    sessions from masterfiles in $VIEVSROOT/DATA/MASTER.
-%    Just process sessions which are in the masterfiles and in the folder
-%    /DATA/NGS/YEAR/.
-%    Attention: mk_list.m works for NGS-files only, vgosDB is not
-%    supported!
+%    The tool has been extended and now supports vgosDB files as well
+%    as VGOS sessions.
 %
 %  Examples:
 %   To get all R1 & R4 sessions run:
@@ -16,6 +17,8 @@
 %             'EXCLUDE','../DATA/MASTER/exclude.txt')
 %   To get all EURO experiments where ONSALA60 has participated in:
 %      [list,sessionnames]=mk_list('EURO','REQSTAT','On')
+%   To get all kind of VGOS sessions of 2019 that already have an OPT-file
+%      [process_list, sess] = mk_list('VGOS', 'ALL_VGOS', 'YEARS', 2019, 'REQOPT')
 %
 % Usage:
 %    [list,sessionnames]=mk_list(s1,s1,...)
@@ -43,6 +46,15 @@
 %       non-intensive sessions
 %    [list,sessionnames]=mk_list(...,'MINSTANUM',4)
 %       Include all available sessions which have 4 or more stations
+%    [list,sessionnames]=mk_list(...,'REQOPT')
+%       Include only sessions for which an OPT file exists.
+% Usage for VGOS:
+%   To include VGOS sessions in the search use 'VGOS'. By default VGOS sessions
+%   aren't included! So 'ALL' won't return VGOS seesions without 'VGOS'. 
+%   Then you can eiter specify the type (eg 'VT') or use all sessions with 'ALL_VGOS':
+%   [process_list, sess] = mk_list('VGOS', 'VT')
+%   [process_list, sess] = mk_list('VGOS', 'ALLVGOS')
+%   [process_list, sess] = mk_list('VGOS', 'ALL')
 %
 % Output:
 %     list      (n*19 string)  List of NGS-files
@@ -59,6 +71,8 @@
 %    23 Aug 2018 by Daniel Landskron: option ONLYINT added
 %    22 Oct 2018 by Daniel Landskron: also enabled for vgosDB
 %    07 Dec 2018 by Daniel Landskron: command window output extended and bug corrected
+%    18 OKT 2019 by Markus Mikschi: implemented VGOS functionality and
+%                                   option to require OPT file
 %
 % ************************************************************************
 
@@ -73,9 +87,10 @@ expes ={};
 reqstat = {};
 all_versions = 0;
 no_intensives = 0;
+no_vgos = 1;
 only_intensives = 0;
 min_station_num = 0;
-
+reqopt = 0;
 
 i_input = 1;
 while i_input <= nargin
@@ -102,6 +117,8 @@ while i_input <= nargin
         case 'MINSTANUM'
             i_input = i_input+1;
             min_station_num = varargin{i_input};
+        case 'REQOPT'
+            reqopt = 1;
         otherwise
             expes = [expes str];
             switch str
@@ -109,6 +126,10 @@ while i_input <= nargin
                     expes = [expes 'EURO'];
                 case 'EURO'
                     expes = [expes 'EUR'];
+            end
+            
+            if strcmp(upper(str), 'VGOS')
+                no_vgos = 0;
             end
     end
     i_input = i_input+1;
@@ -140,19 +161,32 @@ for i_year = 1:length(years)
     else
         path_session = sprintf('../DATA/NGS/%4d/',year);
     end
+    
+    if reqopt
+        path_opt = sprintf('../../VLBI_OPT/VIENNA_VGOSDB/%d/', year);
+    end
         
-    for intf = 0:1
-        if intf
-            if no_intensives==1
-                continue
-            end
-            path_master_file = sprintf('../DATA/MASTER/master%02d-int.txt',year_short);
-        else
-            if only_intensives==1
-                continue
-            end
-            path_master_file = sprintf('../DATA/MASTER/master%02d.txt',year_short);
+    for session_type = 0:2      % 0 = 24h SX, 1 = INT, 2 = VGOS
+        switch session_type
+            case 0 % S/X 24h
+                if only_intensives==1
+                    continue
+                end
+                path_master_file = sprintf('../DATA/MASTER/master%02d.txt',year_short);
+            
+            case 1 % INT
+                if no_intensives==1
+                    continue
+                end
+                path_master_file = sprintf('../DATA/MASTER/master%02d-int.txt',year_short);
+            
+            case 2 % VGOS
+                if no_vgos==1
+                    continue
+                end
+                path_master_file = sprintf('../DATA/MASTER/master%02d-vgos.txt', year_short')
         end
+        
         if exist(path_master_file,'file')
             
             fid = fopen(path_master_file,'r');
@@ -172,6 +206,10 @@ for i_year = 1:length(years)
                             break
                         end
                         if strcmpi(expes{i_expes},'ALL')
+                            incl = 1;
+                        end
+                        
+                        if strcmpi(expes{i_expes},'ALLVGOS') && session_type == 2
                             incl = 1;
                         end
                     end
@@ -202,9 +240,13 @@ for i_year = 1:length(years)
                             if isempty(findstr(reqstat{i_reqstat},tmp))
                                 incl = 0;
                             end
-                        end
-                        
+                        end                        
                     end
+                    
+                    if reqopt && ~isfile(fullfile(path_opt,  [sess '.OPT']))
+                        incl = 0;
+                    end
+                    
                     if incl  &&  (min_station_num>0)
                         tmp = deblank(str(id(7)+1:id(8)-1)); % remove blanks from string
                         find_dash = strfind(tmp, ' -');
@@ -258,7 +300,7 @@ for i_year = 1:length(years)
                         end
                         % remove the unpacked vgosDB folder
                         if strcmpi(session_data_format,'VGOSDB') && exist([path_session sess],'dir')
-                        rmdir([path_session sess], 's');
+                            rmdir([path_session sess], 's');
                         end
                     end
                 end
