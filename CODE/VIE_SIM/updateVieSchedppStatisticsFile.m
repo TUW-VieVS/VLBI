aa8834490b1e8c7bee9d72e55632d527b4ef379e
Matthias Schartner <matthias.schartner@geo.tuwien.ac.at>
2020-01-23 13:14:42 +0100

    bugfix statistics

--- CODE/VIE_SIM/updateVieSchedppStatisticsFile.m
+++ CODE/VIE_SIM/updateVieSchedppStatisticsFile.m
@@ -15,15 +15,15 @@ fclose(fid);
 data = data(idx,:);
 
 simHeader = t_mean_sig.Properties.VariableNames(1:end);
-simHeader = {'n_sim','dUT1_[mus]','x_pol_[muas]','y_pol_[muas]','x_nut_[muas]','y_nut_[muas]', 'average_3d_coordinates_[mm]', simHeader{7:end}};
+simHeader = {'n_sim','dUT1_[mus]','x_pol_[muas]','y_pol_[muas]','x_nut_[muas]','y_nut_[muas]', 'average_3d_coordinates_[mm]', simHeader{9:end}};
 
 mean_sig = t_mean_sig.Variables;
-average = mean(mean_sig(:,7:end),2);
-mean_sig = [mean_sig(:,1:6), average, mean_sig(:,7:end)];
+average = mean(mean_sig(:,9:end),2);
+mean_sig = [mean_sig(:,[1 4:8]), average, mean_sig(:,9:end)];
 
 rep = t_rep.Variables;
-average = mean(rep(:,7:end),2);
-rep = [rep(:,1:6), average, rep(:,7:end)];
+average = mean(rep(:,9:end),2);
+rep = [rep(:,[1 4:8]), average, rep(:,9:end)];
 
 for i=1:length(simHeader)
     header{end+1} = ['vievs_sim_mean_formal_error_' simHeader{i}];
